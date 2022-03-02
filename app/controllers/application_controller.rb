4a3799f7f14a6cc26be077e9471bd8c48f2c7e30
Fernando Ruiz <fernando.ruiz@encora.com>
2022-03-02 17:17:23 -0600

    adding missing files

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,7 @@
 class ApplicationController < ActionController::Base
+  rescue_from ActionController::RoutingError, with: :handle_error_method 
+  
+  def handle_error_method(error)
+    redirect_to action: "index"
+  end
 end
